469e83a388a604d5ea3afb19a0b012ae48e0be12
csh2007 <dnflwlq004@naver.com>
2021-05-29 18:18:28 +0900

    5 commit

--- Practice/21.05.28/main.js
+++ Practice/21.05.28/main.js
@@ -3,7 +3,6 @@ const menu = document.querySelector('.navbar_menu');
 const social = document.querySelector('.navbar_social');
 
 toggleBtn.addEventListener('click',() => {
-
     menu.classList.toggle('active');
     social.classList.toggle('active');
 });
