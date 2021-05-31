5ba2f71e834794ab18fb41ca24e83dd5a6efc991
csh2007 <dnflwlq004@naver.com>
2021-05-31 15:06:54 +0900

    7 commit

--- ReactNative/peticle/src/App.js
+++ ReactNative/peticle/src/App.js
@@ -1,25 +1,35 @@
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faFacebook } from "@fortawesome/free-brands-svg-icons";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+
 
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <nav class="navbar">
+        <div class="navbar_logo">
+            <a href="#"><img class="logo" src="image/peticle_logo.png"/></a>
+            <a href="#">Peticle</a>
+        </div>
+        <ul class="navbar_menu">
+            <li><a href="">회사소개</a></li>
+            <li><a href="">제품소개</a></li>
+            <li><a href="">문의하기</a></li>
+        </ul>
+        <ul class="navbar_social">
+           <li><FontAwesomeIcon icon={faFacebook} /></li>
+           <li><FontAwesomeIcon icon={faInstagram} /></li>
+        </ul>
+        <a href="#" class="navbar_toogleBtn">
+            <FontAwesomeIcon icon={faBars} />
         </a>
-      </header>
-    </div>
+    </nav>
+    
   );
 }
 
