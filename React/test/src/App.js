fd7120698228a813640126b61cd39fbc5e5b36ba
csh2007 <dnflwlq004@naver.com>
2021-06-04 21:19:26 +0900

    8 commit

--- React/test/src/App.js
+++ React/test/src/App.js
@@ -5,17 +5,49 @@ import Content from "./components/Content.js";
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      mode: 'read',
+      subject:{title:'WEB', sub:'World Wide Web!'},
+      welcome:{title:'Welcome', desc:'Hello, React!!'},
+      contents: [
+        {id:1, title:'HTML', desc:'HTML is for information' },
+        {id:2, title:'CSS', desc:'CSS is for design' },
+        {id:3, title:'JavaScript', desc:'JavaScript is for interactive' }
+      ]
+    }
+  }
+
   render(){
+    console.log('App rander');
+    var _title, _desc = null;
+    if(this.state.mode === 'welcome'){
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if(this.state.mode === 'read'){
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].desc;
+    }
     return(
       <div classNmae="App">
-        <Header title="WEB" sub="world wide web!"></Header>
-        <Header title="React" sub="For UI"></Header>
-        <Nav></Nav>
-        <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
+        {/* <Header title={this.state.subject.title} 
+                sub={this.state.subject.sub}>
+
+        </Header> */}
+        <header>
+          <h1><a href="/" onClick={function(e){
+            console.log(e);
+            e.preventDefault();
+            alert('hi');
+          }}>{this.state.subject.title}</a></h1>
+          {this.state.subject.sub}
+        </header>
+        <Nav data={this.state.contents}></Nav>
+        <Content title={_title} desc={_desc}></Content>
       </div>
     );
   }
 }
-
 
 export default App;
